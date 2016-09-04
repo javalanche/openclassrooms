237afc815a10a47314a959c9fd384b4ba2f164a5
Javier Fraga <javier.s.fraga@gmail.com>
2016-09-04 09:26:51 -0700

    completes tutorial todolist

--- my-todolist/app.js
+++ my-todolist/app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var session = require('cookie-sessoin'); // loads the piece of middleware for the session
+var session = require('cookie-session'); // loads the piece of middleware for the session
 var bodyParser	= require('body-parser'); // loads the piece of middleware for managing the settings
 var urlencodedParser = bodyParser.urlencoded(
 	{
@@ -16,19 +16,39 @@ app.use(session({
 	secret: 'todotopsecret'
 }))
 
-.get('/todo', function(req,res){
-	res.setHeader('Content-type','text/plain');
-	res.end();
+.use(function(req,res,next){
+	if (typeof(req.session.todolist) == 'undefined') {
+		req.session.todolist = [];
+	}
+	next();
 })
-.post('/todo/add', function(req,res){
-	res.setHeader('Content-type','text/plain');
-	res.end();
+
+.get('/todo', function(req,res){
+	//res.setHeader('Content-type','text/plain');
+	res.render('todo.ejs',{todolist: req.session.todolist});
+	//res.status(200).send('everything good, homey');
+	//res.end();
+})
+.post('/todo/add', urlencodedParser, function(req,res){
+	//res.setHeader('Content-type','text/plain');
+	console.log("here is the req", req.body);
+	if (req.body.newtodo != '') {
+		req.session.todolist.push(req.body.newtodo);
+	}
+	res.redirect('/todo');
+	//res.end();
 })
 .get('/todo/delete/:id', function(req,res){
-	res.setHeader('Content-type','text/plain');
-	res.end();
+	//res.setHeader('Content-type','text/plain');
+	if (req.params.id != '') {
+		req.session.todolist.splice(req.params.id, 1);
+	}
+	res.redirect('/todo');
+	//res.end();
 })
 .use(function(req,res,next){	
-	res.setHeader('Content-type','text/plain');
+	//res.setHeader('Content-type','text/plain');
+	res.redirect('/todo');
 })
+.listen(8080)
 ;
