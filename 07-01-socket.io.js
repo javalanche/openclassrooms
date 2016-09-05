9cd8dc634342516438271e885ade6a5fbdb30c9b
Javier Fraga <javier.s.fraga@gmail.com>
2016-09-05 12:53:17 -0700

    adds new 07 socket.io files

---
+++ 07-01-socket.io.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var fs = require('fs');
+
+
+// loading the index file. html is displayed to the client
+var server = http.createServer( function (req,res) {
+	
+	fs.readFile('./index.html','utf-8', function (error,content) {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.end(content);
+	});
+});
+
+// loading socket.io 
+var io = require('socket.io').listen(server);
+
+// when a client connects, we note it in the console
+io.sockets.on('connection', function (socket) {
+	/*
+	 *connection confirmation 
+	 */
+	console.log('A client is connected!');	
+	socket.emit('message','You are connected!');
+	socket.emit('message', { content: 'hi javier, sent with object', importance: '1' });
+});
+
+server.listen(8080);
