9699980c1fd8edb85be9ad7a6e58aef48cb5a4c1
Javier Fraga <javier.s.fraga@gmail.com>
2016-09-06 14:18:29 -0700

    adds broadcast example

--- 07-01-socket.io.js
+++ 07-01-socket.io.js
@@ -22,6 +22,7 @@ io.sockets.on('connection', function (socket) {
 	console.log('A client is connected!');	
 	socket.emit('message','You are connected!');
 	socket.emit('message', { content: 'hi javier, sent with object', importance: '1' });
+	socket.broadcast.emit('message','Another client has just connected.');
 
 	socket.on('message', function (message) {
 		console.log('A client is speaking to me. They\'re saying: ', message);
